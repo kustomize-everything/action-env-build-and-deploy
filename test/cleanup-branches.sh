f39e8deb82831039a2a65095792e4d9f3b188176
Brandon High <highb@users.noreply.github.com>
2023-02-27 16:50:35 -0800

    Eh, maybe let's not auto clean branches

--- test/cleanup-branches.sh
+++ test/cleanup-branches.sh
@@ -10,10 +10,4 @@ fi
 if git ls-remote --exit-code --heads origin deploy-pr/env-dev; then
 git push --delete origin deploy-pr/env-dev
 fi
-
-# Delete all origin branches prefixed with pr/
-for branch in $(git ls-remote --heads origin | grep -o 'pr/.*' | cut -d '/' -f 2); do
-  git push --delete origin "pr/${branch}"
-done
-
 set -e
