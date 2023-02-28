cc8d868c009e56aac307185e87c929cfd040cde2
Brandon High <highb@users.noreply.github.com>
2023-02-27 17:57:28 -0800

    alwasy delete

--- test/cleanup-branches.sh
+++ test/cleanup-branches.sh
@@ -4,11 +4,5 @@ REPO="$1"
 
 cd "${REPO}" || exit 1
 set +e
-if git ls-remote --exit-code --heads origin refs/heads/env-dev; then
-    set -e
-    git push --delete origin env-dev
-fi
-if git ls-remote --exit-code --heads origin refs/heads/deploy-pr/env-dev; then
-    set -e
-    git push --delete origin deploy-pr/env-dev
-fi
+git push --delete origin env-dev
+git push --delete origin deploy-pr/env-dev
