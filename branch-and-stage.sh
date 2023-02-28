259f9336794bc356b5874091071f8c8387ae594d
Brandon High <highb@users.noreply.github.com>
2023-02-27 18:54:53 -0800

    Split out bash scripts from action.yml

---
+++ branch-and-stage.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Output all commands
+set -x
+
+# Show line numbers
+export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+
+# Base changes off the branch being deployed to
+set +e
+# If the branch exists, check it out
+if git ls-remote --exit-code --heads origin "refs/heads/${ENV_BRANCH}"; then
+  git checkout "${ENV_BRANCH}" --
+else
+# If the branch does not exist, create it
+  git checkout --orphan "${ENV_BRANCH}" --
+  git rm -rf --ignore-unmatch '*'
+  set -e
+  # Ensure that branch will not be polluted with unrendered YAML
+  rm -rf base/ env/
+  git commit --allow-empty -m "Initial Commit"
+  git push origin "${ENV_BRANCH}"
+fi
+set -e
+
+git checkout -B "${PUSH_BRANCH}" --
+
+echo "Cleaning staging area..."
+git rm -rf --ignore-unmatch '*'
+# Ensure that branch will not be polluted with unrendered YAML
+rm -rf base/ env/
+echo "Post-staging cleanup status:"
+git status
+echo "Moving built k8s-manifests into staging area..."
+cp /tmp/*.y*ml .
+git add --all -fv ./*.y*ml
--- kustomize-build.sh
+++ kustomize-build.sh
@@ -3,12 +3,6 @@
 # Fail on non-zero exit
 set -e
 
-# Output all commands
-set -x
-
-# Show line numbers
-export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
-
 # Automatically add meta annotations at build-time
 pushd "${ENV_DIR}" || exit 1
 kustomize edit add annotation env-branch:"${ENV_BRANCH}"
@@ -28,32 +22,3 @@ popd || exit 1
 
 # Must reset to clear build-time annotations
 git reset --hard
-
-# Base changes off the branch being deployed to
-set +e
-# If the branch exists, check it out
-if git ls-remote --exit-code --heads origin "refs/heads/${ENV_BRANCH}"; then
-  git checkout "${ENV_BRANCH}" --
-else
-# If the branch does not exist, create it
-  git checkout --orphan "${ENV_BRANCH}" --
-  git rm -rf --ignore-unmatch '*'
-  set -e
-  # Ensure that branch will not be polluted with unrendered YAML
-  rm -rf base/ env/
-  git commit --allow-empty -m "Initial Commit"
-  git push origin "${ENV_BRANCH}"
-fi
-set -e
-
-git checkout -B "${PUSH_BRANCH}" --
-
-echo "Cleaning staging area..."
-git rm -rf --ignore-unmatch '*'
-# Ensure that branch will not be polluted with unrendered YAML
-rm -rf base/ env/
-echo "Post-staging cleanup status:"
-git status
-echo "Moving built k8s-manifests into staging area..."
-cp /tmp/*.y*ml .
-git add --all -fv ./*.y*ml
